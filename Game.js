ab4f876747fd5b07bd3c096da62a501b2c4e6d92
Markus Ankenbrand <markus@ankenbrand.me>
2017-03-29 16:01:51 +0200

    Update standing on button click

--- Game.js
+++ Game.js
@@ -13,6 +13,13 @@ import {
 } from 'react-native';
 import MyButton from './MyButton';
 
+
+class MyText extends Component {
+  render(){
+    return <Text>{this.props.standing[0]} - {this.props.standing[1]}</Text>
+  }
+};
+
 export class Game extends Component {
   static navigationOptions = {
     header: {visible: false},
@@ -22,7 +29,8 @@ export class Game extends Component {
     const {teams, game} = props.navigation.state.params;
     this.state = {
       teams: teams,
-      game: game
+      game: game,
+      standing: [0,0]
     }
   }
 
@@ -35,13 +43,16 @@ export class Game extends Component {
         <Text>
           Game {this.state.game}: Schnick-Schnack-Schnuck
         </Text>
+        <MyText
+          standing={this.state.standing}
+        />
         <MyButton
           text={this.state.teams[0][Math.floor(Math.random()*this.state.teams[0].length)]}
-          onPress={()=>console.log("blue")}
+          onPress={()=>this.setState({standing: [this.state.standing[0]+1,this.state.standing[1]]})}
         />
         <MyButton
           text={this.state.teams[1][Math.floor(Math.random()*this.state.teams[1].length)]}
-          onPress={()=>console.log("red")}
+          onPress={()=>this.setState({standing: [this.state.standing[0],this.state.standing[1]+1]})}
         />
       </View>
     );
