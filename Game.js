45cad386c58daa9237acfcad2ccfa296666e7b3f
Markus Ankenbrand <markus@ankenbrand.me>
2017-04-13 13:11:23 +0200

    Improve game layout

--- Game.js
+++ Game.js
@@ -12,7 +12,16 @@ import MyButton from './MyButton';
 
 class MyText extends Component {
   render(){
-    return <Text>{this.props.standing[0]} - {this.props.standing[1]}</Text>
+    return(
+      <View style={styles.buttonView}>
+        <View style={styles.playerButtonView}>
+          <Text style={styles.standingText}>{this.props.standing[0]}</Text>
+        </View>
+        <View style={styles.playerButtonView}>
+          <Text style={styles.standingText}>{this.props.standing[1]}</Text>
+        </View>
+      </View>
+    );
   }
 };
 
@@ -50,38 +59,43 @@ export class Game extends Component {
     const { navigate } = this.props.navigation
 
     return (
-      <View>
-        <Text style={styles.title}>
-          Game {score.length+1}: {game.name}
-        </Text>
-        <Text>
-          (best of {game.bestOf})
-        </Text>
-        <MyButton
-          text='Instructions'
-          onPress={()=>Alert.alert( 'Instructions', this.state.game.instructions, [ {text: 'OK', onPress: () => {}} ], { cancelable: true } )}
-        />
-        <MyText
-          standing={this.state.standing}
-        />
-        <View style={styles.buttonView}>
-          <View style={styles.playerButtonView}>
-          <MyButton
-            text={this.state.teams[0][Math.floor(Math.random()*this.state.teams[0].length)]}
-            onPress={()=>this.handleScore(0)}
+      <View style={styles.main}>
+        <View></View>
+        <View style={styles.content}>
+          <Text style={styles.title}>
+            Game {score.length+1}: {game.name}
+          </Text>
+          <Text alignSelf="center">
+            (best of {game.bestOf})
+          </Text>
+          <MyText
+            standing={this.state.standing}
           />
-          </View>
-          <View style={styles.playerButtonView}>
-          <MyButton
-            text={this.state.teams[1][Math.floor(Math.random()*this.state.teams[1].length)]}
-            onPress={()=>this.handleScore(1)}
-          />
+          <View style={styles.buttonView}>
+            <View style={styles.playerButtonView}>
+            <MyButton
+              text={this.state.teams[0][Math.floor(Math.random()*this.state.teams[0].length)]}
+              onPress={()=>this.handleScore(0)}
+            />
+            </View>
+            <View style={styles.playerButtonView}>
+            <MyButton
+              text={this.state.teams[1][Math.floor(Math.random()*this.state.teams[1].length)]}
+              onPress={()=>this.handleScore(1)}
+            />
+            </View>
           </View>
         </View>
-        <MyButton
-          text="Skip this game"
-          onPress={()=>Alert.alert( 'Skip game', "Are you sure you want to skip this game? The next one will start with 0-0.", [ {text: 'Yes', onPress: () => {navigate('Standing', {teams: this.state.teams, score: this.state.score})}}, {text: 'No', onPress: () => {}} ], { cancelable: true } )}
-        />
+        <View>
+          <MyButton
+            text='Instructions'
+            onPress={()=>Alert.alert( 'Instructions', this.state.game.instructions, [ {text: 'OK', onPress: () => {}} ], { cancelable: true } )}
+          />
+          <MyButton
+            text="Skip this game"
+            onPress={()=>Alert.alert( 'Skip game', "Are you sure you want to skip this game? The next one will start with 0-0.", [ {text: 'Yes', onPress: () => {navigate('Standing', {teams: this.state.teams, score: this.state.score})}}, {text: 'No', onPress: () => {}} ], { cancelable: true } )}
+          />
+        </View>
       </View>
     );
   }
@@ -100,6 +114,15 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 5
   },
-  playerButton: {
+  standingText: {
+    alignSelf: 'center',
+    fontSize: 72
+  },
+  content: {
+    flex: 1,
+  },
+  main: {
+    flex: 1,
+    margin: 5,
   }
 });
