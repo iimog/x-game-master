8606cbe97009bb62ded85771019193b20cb2c971
Markus Ankenbrand <markus@ankenbrand.me>
2017-04-17 20:09:58 +0200

    Add basic redux capability

--- app/index.js
+++ app/index.js
@@ -2,11 +2,13 @@
  * @flow
  */
 
-import React from 'react';
+import React, {Component} from 'react';
 import {
   AppRegistry,
   Text,
 } from 'react-native';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import { StackNavigator } from 'react-navigation';
 import Home from './routes/Home';
 import PlayerSelect from './routes/PlayerSelect';
@@ -14,8 +16,9 @@ import TeamSelect from './routes/TeamSelect';
 import Game from './routes/Game';
 import Score from './routes/Score';
 import FinalScore from './routes/FinalScore';
+import { reducer } from './redux'
 
-const xMobile = StackNavigator({
+const AppNavigator = StackNavigator({
   Home: { screen: Home },
   NewGame: { screen: PlayerSelect },
   Team: { screen: TeamSelect },
@@ -23,5 +26,17 @@ const xMobile = StackNavigator({
   Score: { screen: Score },
   FinalScore: { screen: FinalScore },
 });
+
+const store = createStore(reducer);
+
+class xMobile extends Component{
+  render(){
+    return(
+      <Provider store={store}>
+        <AppNavigator />
+      </Provider>
+    )
+  }
+}
 
 AppRegistry.registerComponent('xMobile', () => xMobile);
