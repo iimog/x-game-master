90c9179c11f36ae11c5042432064f101587bcf62
Markus Ankenbrand <markus@ankenbrand.me>
2017-04-17 20:52:58 +0200

    Add teams to global store and shuffle action

--- app/redux/index.js
+++ app/redux/index.js
@@ -4,6 +4,7 @@
 export const types = {
   ADD: 'ADD',
   REMOVE: 'REMOVE',
+  SHUFFLE: 'SHUFFLE'
 }
 
 // Helper functions to dispatch actions, optionally with payloads
@@ -13,12 +14,32 @@ export const actionCreators = {
   },
   remove: (index: number) => {
     return {type: types.REMOVE, payload: index}
-  }
+  },
+  shuffle: () => {
+    return {type: types.SHUFFLE, payload: null}
+  },
 }
 
 // Initial state of the store
 const initialState = {
   players: ['Hannah', 'Markus', 'Tobi', 'Lo', 'Moritz'],
+  teams: [[0,1,2],[3,4]]
+}
+
+/**
+ * Shuffles array in place. ES6 version
+ * @param {Array} a items The array containing the items.
+ */
+const shuffle = <T>(a: Array<T>): Array<T> => {
+    let b = [...a];
+    for (let i = b.length; i; i--) {
+        let j = Math.floor(Math.random() * i);
+        // the next three lines could be replaced by [b[i - 1]b[j]] = [b[j], b[i - 1]] but flow (v0.38.0) does not like it
+        let [x,y] = [b[j], b[i - 1]];
+        b[i - 1] = x;
+        b[j] = y;
+    }
+    return b;
 }
 
 // Function to handle actions and update the state of the store.
@@ -29,20 +50,37 @@ const initialState = {
 //   call reducer() with no state on startup, and we are expected to
 //   return the initial state of the app in this case.
 export const reducer = (state: typeof initialState = initialState, action: {type: string, payload: any}) => {
-  const {players} = state
+  const {players, teams} = state
   const {type, payload} = action
 
   switch (type) {
     case types.ADD: {
+      let p = [payload, ...players]
+      let t = Array.from({length: p.length}, (value, key) => key)
+      t = [t.slice(0, Math.ceil(t.length)),t.slice(Math.ceil(t.length))]
       return {
         ...state,
-        players: [payload, ...players],
+        players: p,
+        teams: t
       }
     }
     case types.REMOVE: {
+      let p = players.filter((todo, i) => i !== payload)
+      let t = Array.from({length: p.length}, (value, key) => key)
+      t = [t.slice(0, Math.ceil(t.length)),t.slice(Math.ceil(t.length))]
       return {
         ...state,
-        players: players.filter((todo, i) => i !== payload),
+        players: p,
+        teams: t
+      }
+    }
+    case types.SHUFFLE: {
+      let t = Array.from({length: players.length}, (value, key) => key)
+      t = shuffle(t)
+      t = [t.slice(0, Math.ceil(t.length)),t.slice(Math.ceil(t.length))]
+      return {
+        ...state,
+        teams: t
       }
     }
   }
