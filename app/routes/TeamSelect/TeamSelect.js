078d312a10aa2df8120f6ccc99ecf3b0b68f939f
Markus Ankenbrand <markus@ankenbrand.me>
2017-04-27 14:16:42 +0200

    Improve teamSelect layout

--- app/routes/TeamSelect/TeamSelect.js
+++ app/routes/TeamSelect/TeamSelect.js
@@ -14,6 +14,7 @@ import TeamList from '../../components/TeamList';
 import styles from './styles';
 import I18n from '../../i18n'
 import { shuffleTeams } from '../../lib'
+import layout from '../../layouts'
 
 const mapStateToProps = (store) => ({players: store.players, teams: store.teams, matchSettings: store.matchSettings})
 
@@ -26,24 +27,26 @@ class TeamSelect extends Component {
     const { navigate } = this.props.navigation;
     const { players, teams } = this.props;
     return (
-      <ScrollView>
-        <TeamList
-          title={I18n.t('team1')}
-          list={teams[0].map(i => players[i])}
-          color="skyblue"
-        />
-        <TeamList
-          title={I18n.t('team2')}
-          list={teams[1].map(i => players[i])}
-          color="red"
-        />
+      <View style={layout.main}>
+        <ScrollView style={layout.content}>
+          <TeamList
+            title={I18n.t('team1')}
+            list={teams[0].map(i => players[i])}
+            color="skyblue"
+          />
+          <TeamList
+            title={I18n.t('team2')}
+            list={teams[1].map(i => players[i])}
+            color="red"
+          />
+        </ScrollView>
         <View style={styles.buttons}>
           <Button onPress={()=>{this.props.dispatch(actionCreators.setTeams(shuffleTeams(teams)))}} text={I18n.t('shuffle')}/>
           <Button onPress={()=>{
               navigate('Score')
           }} text={I18n.t('start')}/>
         </View>
-      </ScrollView>
+      </View>
     );
   }
 };
--- app/routes/TeamSelect/styles.js
+++ app/routes/TeamSelect/styles.js
@@ -3,7 +3,6 @@ import { StyleSheet } from 'react-native';
 
 const styles: StyleSheet.Styles = StyleSheet.create({
   buttons: {
-    flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-between',
     marginTop: 10
