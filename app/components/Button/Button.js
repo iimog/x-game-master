59f238b9496749a192ee719e9a090bc26261da24
Markus Ankenbrand <markus@ankenbrand.me>
2017-04-28 09:16:32 +0200

    Add (optional) icon to buttons

--- app/components/Button/Button.js
+++ app/components/Button/Button.js
@@ -6,12 +6,17 @@ import {
   Text
 } from 'react-native';
 import styles from './styles';
+import FontAwesome, { Icons } from 'react-native-fontawesome'
 
 export default class Button extends Component {
   render() {
+    let icon: ?FontAwesome = null
+    if(this.props.icon !== null){
+      icon = <FontAwesome>{Icons[this.props.icon]}</FontAwesome>
+    }
     return (
       <TouchableHighlight onPress={this.props.onPress} style={styles.button}>
-        <Text style={styles.text}>{this.props.text}</Text>
+        <Text style={styles.text}>{icon}{this.props.text}</Text>
       </TouchableHighlight>
     );
   }
