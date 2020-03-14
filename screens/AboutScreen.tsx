63eacd0ef35fe68661ea85eb9d74d7a77817a67c
Markus Ankenbrand <markus@ankenbrand.me>
2020-03-14 22:28:44 +0100

    Fix permissions and app data

--- screens/AboutScreen.tsx
+++ screens/AboutScreen.tsx
@@ -30,7 +30,7 @@ class AboutScreen extends React.Component<{navigation},{}> {
             <Text>
               For a description of how to play the game please see: <Text style={{color: 'blue'}}
               onPress={() => Linking.openURL('https://github.com/iimog/x-game-master')}>
-                X Game Manager Website
+                X Game Manager Website.{" "}
               </Text>
               This app is open source, please report bugs and suggestions at the website as well.
               
