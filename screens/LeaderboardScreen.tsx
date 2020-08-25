72861bdcba8e69d1ddee64ea7d8ed2a6d5c53b01
Markus Ankenbrand <markus@ankenbrand.me>
2020-08-25 21:50:44 +0200

    Add strict ts and RTK

--- screens/LeaderboardScreen.tsx
+++ screens/LeaderboardScreen.tsx
@@ -1,205 +1,209 @@
 import React from "react";
 import { StyleSheet, View, Dimensions, Alert } from "react-native";
 import { Layout, Button, Text, List, ListItem, Icon, Tab, TabView } from "@ui-kitten/components";
-import { Player, Round } from "../store";
-import { connect } from 'react-redux';
+import { Player, Round, actions } from "../store";
+import { connect, ConnectedProps } from 'react-redux';
 import { ThemedSafeAreaView } from "../components/ThemedSafeAreaView";
-import _ from "lodash";
+import _, { fromPairs } from "lodash";
 
-class LeaderboardScreen extends React.Component<{navigation, dispatch, players: Array<Player>, rounds: Array<Round>, games: Array<string>},{selectedIndex: number}> {
-    constructor(props) {
-      super(props);
-      this.state = {
-        selectedIndex: 0,
-      };
-    }
-    getPlayerScores: () => { [key: string]: number; } = () => {
-      let playerScores = {};
-      const {rounds, players} = this.props;
-      players.map(x => playerScores[x.name] = 0);
-      for(let i=0; i<rounds.length; i++){
-        let round = rounds[i]
-       if(round.winner in [0,1]){
-          round.teams[round.winner].map(player => playerScores[player.name]+=(i+1));
-       }
+class LeaderboardScreen extends React.Component<NavigationStackScreenProps & PropsFromRedux, { selectedIndex: number }> {
+  state = { selectedIndex: 0 }
+
+  getPlayerScores = () => {
+    let playerScores: { [player: string]: number } = {};
+    const { rounds, players } = this.props;
+    players.map(x => playerScores[x.name] = 0);
+    for (let i = 0; i < rounds.length; i++) {
+      let round = rounds[i]
+      if (round.winner === 0 || round.winner === 1) {
+        round.teams[round.winner].map(player => playerScores[player.name] += (i + 1));
       }
-      return playerScores;
     }
-    getRandomTeams: (players: Array<Player>) => Array<Array<Player>> = (players) => {
-      let activePlayers = players.filter(p => p.active)
-      let shuffledPlayers = _.shuffle(activePlayers);
-      let splitPoint = Math.floor(activePlayers.length/2)
-      if(activePlayers.length % 2){
-        splitPoint += _.random();
-      }
-      let teams = [shuffledPlayers.slice(0,splitPoint), shuffledPlayers.slice(splitPoint)]
-      return teams;
+    return playerScores;
+  }
+  getRandomTeams= (players: Array<Player>) => {
+    let activePlayers = players.filter(p => p.active)
+    let shuffledPlayers = _.shuffle(activePlayers);
+    let splitPoint = Math.floor(activePlayers.length / 2)
+    if (activePlayers.length % 2) {
+      splitPoint += _.random();
     }
-    render() {
-      const {navigate} = this.props.navigation;
-      const fullWidth = Dimensions.get('window').width;
-      const gameIndex = this.props.rounds.length-1;
-      const gameRunning = gameIndex >= 0 && this.props.rounds[gameIndex].winner == -1;
-      let playerScores = this.getPlayerScores();
-      let isOver = this.props.rounds.length >= this.props.games.length && !gameRunning;
-      let sortedPlayers = this.props.players.sort((x,y) => playerScores[y.name]-playerScores[x.name])
-      let lastScore = -1;
-      let tieIndex = -1;
-      let ranks = sortedPlayers.map((player,index) => {
-        let thisScore = playerScores[player.name];
-        if(thisScore == lastScore){
-          return (tieIndex+1).toString();
-        } else {
-          lastScore = thisScore;
-          tieIndex = index;
-          return (index+1).toString();
-        }
-      });
-      if(isOver){
-        ranks = ranks.map(x => x=='1' ? '🏆' : x);
+    let teams = [shuffledPlayers.slice(0, splitPoint), shuffledPlayers.slice(splitPoint)] as const
+    return teams;
+  }
+  render() {
+    const { navigate } = this.props.navigation;
+    const fullWidth = Dimensions.get('window').width;
+    const gameIndex = this.props.rounds.length - 1;
+    const gameRunning = gameIndex >= 0 && this.props.rounds[gameIndex].winner == -1;
+    let playerScores = this.getPlayerScores();
+    let isOver = this.props.rounds.length >= this.props.games.length && !gameRunning;
+    let sortedPlayers = this.props.players.sort((x, y) => playerScores[y.name] - playerScores[x.name])
+    let lastScore = -1;
+    let tieIndex = -1;
+    let ranks = sortedPlayers.map((player, index) => {
+      let thisScore = playerScores[player.name];
+      if (thisScore == lastScore) {
+        return (tieIndex + 1).toString();
+      } else {
+        lastScore = thisScore;
+        tieIndex = index;
+        return (index + 1).toString();
       }
-      let remainingGames = _.shuffle(this.props.games.slice(this.props.rounds.length));
-      return (
-        <ThemedSafeAreaView>
-              <TabView
-      selectedIndex={this.state.selectedIndex}
-      onSelect={(index) => this.setState({selectedIndex: index})}
-      style={{flex: 1}}>
-      <Tab title='Leaderboard'>
-        <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-          <View style={{width: fullWidth, padding: 15}}> 
-            <List
-              data={sortedPlayers}
-              renderItem={({item, index}) => {
-                return(
-                  <ListItem onPress={(index) => {
-                    this.props.dispatch({type: 'TOGGLE_PLAYER', payload: sortedPlayers[index].name})
-                  }}>
-                    <LeaderboardEntry rank={ranks[index]} name={item.name} points={playerScores[item.name]} active={item.active}/>
-                  </ListItem>
-                )}
-              }
-              keyExtractor={item => item.name}
-            />
-          </View>
-        </Layout>
-      </Tab>
-      <Tab title='Games'>
-      <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-          <View style={{width: fullWidth, padding: 15}}> 
-            <List
-              data={this.props.rounds}
-              renderItem={({item, index}) => {
-                return(
-                  <ListItem key={index} onLongPress={(index) => {
-                    Alert.alert(
-                      'Rewrite history?',
-                      'Change winner or remove (no undo for that)?',
-                      [
-                        {
-                          text: 'Change winner',
-                          onPress: () => this.props.dispatch({type: 'TOGGLE_GAME_RESULT', payload: index})
-                        },
-                        {
-                          text: 'Remove',
-                          onPress: () => this.props.dispatch({type: 'REMOVE_GAME', payload: index}),
-                          style: 'destructive'
-                        },
-                        {
-                          text: 'Cancel',
-                          style: 'cancel',
-                        },
-                      ],
-                      {cancelable: true},
-                    );
-                    
-                  }}>
-                      <GameListEntry gameIndex={index} name={item.game} winner={item.winner} teams={item.teams}/>
-                  </ListItem>
-                )}
-              }
-            />
-            <Text category="h4" appearance="hint" style={{marginTop:25}}>{remainingGames.length>0 ? "Remaining games:" : ""}</Text>
-            <List
-              data={remainingGames}
-              renderItem={({item, index}) => {
-                return(
-                  <ListItem key={index}>
-                      <UnplayedGameListEntry name={item}/>
-                  </ListItem>
-                )}
-              }
-            />
-          </View>
-        </Layout>
-      </Tab>
-    </TabView>
-    <Button onPress={() => {
-                  if(isOver){
-                    navigate('Main');
-                  } else {
-                    if(!gameRunning){
-                      let teams = this.getRandomTeams(this.props.players)
-                      this.props.dispatch({type: 'START_NEXT_GAME', payload: teams})
-                    }
-                    navigate('Game')
+    });
+    if (isOver) {
+      ranks = ranks.map(x => x == '1' ? '🏆' : x);
+    }
+    let remainingGames = _.shuffle(this.props.games.slice(this.props.rounds.length));
+    return (
+      <ThemedSafeAreaView>
+        <TabView
+          selectedIndex={this.state.selectedIndex}
+          onSelect={(index) => this.setState({ selectedIndex: index })}
+          style={{ flex: 1 }}>
+          <Tab title='Leaderboard'>
+            <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+              <View style={{ width: fullWidth, padding: 15 }}>
+                <List
+                  data={sortedPlayers}
+                  renderItem={({ item, index }: {item: Player, index: number}) => {
+                    return (
+                      <ListItem onPress={(index) => {
+                        this.props.dispatch({ type: 'TOGGLE_PLAYER', payload: sortedPlayers[index].name })
+                      }}>
+                        <LeaderboardEntry rank={ranks[index]} name={item.name} points={playerScores[item.name]} active={item.active} />
+                      </ListItem>
+                    )
                   }
-                }
-              }>
-              {isOver ? "Back to Main" : (gameRunning ? "Back to Game" : "Next Game")}
-            </Button>
-        </ThemedSafeAreaView>
-      );
-    }
-  }
-export default connect(state => state)(LeaderboardScreen)
+                  }
+                  keyExtractor={item => item.name}
+                />
+              </View>
+            </Layout>
+          </Tab>
+          <Tab title='Games'>
+            <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+              <View style={{ width: fullWidth, padding: 15 }}>
+                <List
+                  data={this.props.rounds}
+                  renderItem={({ item, index }) => {
+                    return (
+                      <ListItem key={index} onLongPress={(index) => {
+                        Alert.alert(
+                          'Rewrite history?',
+                          'Change winner or remove (no undo for that)?',
+                          [
+                            {
+                              text: 'Change winner',
+                              onPress: () => this.props.dispatch({ type: 'TOGGLE_GAME_RESULT', payload: index })
+                            },
+                            {
+                              text: 'Remove',
+                              onPress: () => this.props.dispatch({ type: 'REMOVE_GAME', payload: index }),
+                              style: 'destructive'
+                            },
+                            {
+                              text: 'Cancel',
+                              style: 'cancel',
+                            },
+                          ],
+                          { cancelable: true },
+                        );
 
-class LeaderboardEntry extends React.Component<{rank: string, name: string, points: number, active: boolean}, {}> {
-    render() {
-      const appearance = this.props.active ? "default" : "hint";
-      return (
-        <View style={styles.lbEntryContainer}>
-          <Text category="h3" appearance={appearance}>{this.props.rank}</Text>
-          <View style={{flex: 1, marginLeft: 10}}><Text category="h3" appearance={appearance}>{this.props.name}</Text></View>
-          <Text style={{marginRight: 10}} category="h3" appearance={appearance}>{this.props.points.toString()}</Text>
-          <Icon name={this.props.active ? 'checkmark-circle-2' : 'checkmark-circle-2-outline'} width={25} height={25} fill="#fff"/>
-        </View>
-      );
-    }
+                      }}>
+                        <GameListEntry gameIndex={index} name={item.game} winner={item.winner} teams={item.teams} />
+                      </ListItem>
+                    )
+                  }
+                  }
+                />
+                <Text category="h4" appearance="hint" style={{ marginTop: 25 }}>{remainingGames.length > 0 ? "Remaining games:" : ""}</Text>
+                <List
+                  data={remainingGames}
+                  renderItem={({ item, index }) => {
+                    return (
+                      <ListItem key={index}>
+                        <UnplayedGameListEntry name={item} />
+                      </ListItem>
+                    )
+                  }
+                  }
+                />
+              </View>
+            </Layout>
+          </Tab>
+        </TabView>
+        <Button onPress={() => {
+          if (isOver) {
+            navigate('Main');
+          } else {
+            if (!gameRunning) {
+              let teams = this.getRandomTeams(this.props.players)
+              this.props.startNextGame({teams})
+            }
+            navigate('Game')
+          }
+        }
+        }>
+          {isOver ? "Back to Main" : (gameRunning ? "Back to Game" : "Next Game")}
+        </Button>
+      </ThemedSafeAreaView>
+    );
   }
+}
+import { State } from '../store'
+import { NavigationStackScreenProps } from "react-navigation-stack";
+const connector = connect((state: State) => state, actions)
+type PropsFromRedux = ConnectedProps<typeof connector>
 
-class GameListEntry extends React.Component<{gameIndex: number, name: string, winner: number, teams: Array<Array<Player>>}, {}> {
-    render() {
-      const appearance = this.props.winner >= 0 ? "default" : "hint";
-      return (
-        <View style={styles.lbEntryContainer}>
-          <Text category="h3" appearance={appearance}>{(this.props.gameIndex+1).toString()}.</Text>
-          <View style={{flex: 2, marginLeft: 10}}><Text category="h3" appearance={appearance}>{this.props.name}</Text></View>
-          <Text style={{flex: 1, marginRight: 10}}>
-            { this.props.winner<0 ? '...running...' : this.props.teams[this.props.winner].map(x => x.name).join(", ")}
-          </Text>
-        </View>
-      );
-    }
-  }
+export default connector(LeaderboardScreen)
 
-class UnplayedGameListEntry extends React.Component<{name: string}, {}> {
-    render() {
-      const appearance = "hint";
-      return (
-        <View style={styles.lbEntryContainer}>
-          <Text category="h3" appearance={appearance}>?.</Text>
-          <View style={{flex: 1, marginLeft: 10}}><Text category="h3" appearance={appearance}>{this.props.name}</Text></View>
-        </View>
-      );
-    }
+class LeaderboardEntry extends React.Component<{ rank: string, name: string, points: number, active: boolean }, {}> {
+  render() {
+    const appearance = this.props.active ? "default" : "hint";
+    return (
+      <View style={styles.lbEntryContainer}>
+        <Text category="h3" appearance={appearance}>{this.props.rank}</Text>
+        <View style={{ flex: 1, marginLeft: 10 }}><Text category="h3" appearance={appearance}>{this.props.name}</Text></View>
+        <Text style={{ marginRight: 10 }} category="h3" appearance={appearance}>{this.props.points.toString()}</Text>
+        <Icon name={this.props.active ? 'checkmark-circle-2' : 'checkmark-circle-2-outline'} width={25} height={25} fill="#fff" />
+      </View>
+    );
   }
-  
+}
+
+class GameListEntry extends React.Component<{ gameIndex: number, name: string, winner: number, teams: Array<Array<Player>> }, {}> {
+  render() {
+    const appearance = this.props.winner >= 0 ? "default" : "hint";
+    return (
+      <View style={styles.lbEntryContainer}>
+        <Text category="h3" appearance={appearance}>{(this.props.gameIndex + 1).toString()}.</Text>
+        <View style={{ flex: 2, marginLeft: 10 }}><Text category="h3" appearance={appearance}>{this.props.name}</Text></View>
+        <Text style={{ flex: 1, marginRight: 10 }}>
+          {this.props.winner < 0 ? '...running...' : this.props.teams[this.props.winner].map(x => x.name).join(", ")}
+        </Text>
+      </View>
+    );
+  }
+}
+
+class UnplayedGameListEntry extends React.Component<{ name: string }, {}> {
+  render() {
+    const appearance = "hint";
+    return (
+      <View style={styles.lbEntryContainer}>
+        <Text category="h3" appearance={appearance}>?.</Text>
+        <View style={{ flex: 1, marginLeft: 10 }}><Text category="h3" appearance={appearance}>{this.props.name}</Text></View>
+      </View>
+    );
+  }
+}
+
 const styles = StyleSheet.create({
-    lbEntryContainer: {
-      flex: 1,
-      flexDirection: "row",
-      alignItems: 'center',
-      justifyContent: 'space-between',
-    },
-  });
+  lbEntryContainer: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+});
--- store/index.tsx
+++ store/index.tsx
@@ -1,8 +1,9 @@
-import { createStore, StoreCreator } from 'redux';
+import { createStore, StoreCreator } from 'redux'
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 //import AsyncStorage from '@react-native-community/async-storage';
-import { AsyncStorage } from 'react-native';
-import { persistStore, persistReducer } from 'redux-persist';
-import _ from 'lodash';
+import { AsyncStorage } from 'react-native'
+import { persistStore, persistReducer } from 'redux-persist'
+import _ from 'lodash'
 
 const persistConfig = {
   key: 'root',
@@ -20,9 +21,9 @@ export type Round = {
   winner: -1 | 0 | 1 | null
 }
 
-type State = {
+export type State = {
     players: Array<Player>,
-    games: Array<String>,
+    games: Array<string>,
     rounds: Array<Round>,
     matchId: number,
     lastChange: number,
@@ -36,79 +37,75 @@ const INITIAL_STATE: State = {
   lastChange: -1,
 };
 
+function withTime<T>(reducer: (state: State, action: PayloadAction<T & { time: number }>)=>any){
+ return  {
+   reducer,
+   prepare(payload: T){
+     return { payload: {...payload, time: Date.now()}}
+   }
+ }
+}
+
 // TODO can I check payload types?
-const matchReducer = (state = INITIAL_STATE, action) => {
-  switch (action.type) {
-    case 'RESET':
-        return {...state,
-          rounds: [],
-          lastChange: -1,
-          matchId: -1,
-      };
-    case 'START_MATCH':
-        // Start new game with players and games (replacing old state)
-        return {
-            players: action.payload.players,
-            games: action.payload.games,
-            rounds: [],
-            matchId: Date.now(),
-            lastChange: Date.now(),
-        };
-    case 'LOAD_MATCH':
-        // Load match with state from payload (replacing old state)
-        return action.payload;
-    case 'GAME_RESULT':
-        // Set result of this game
-        let rounds = _.cloneDeep(state.rounds);
-        rounds[rounds.length-1].winner = action.payload;
-        return {...state, 
-            rounds: rounds,
-            lastChange: Date.now(),
-        };
-    case 'START_NEXT_GAME':
-        // Add result of this game to previous rounds
-        return {...state,
-            rounds: [...state.rounds, {
-              teams: action.payload,
-              game: state.games[state.rounds.length],
-              winner: -1
-            }],
-            lastChange: Date.now(),
-        };
-    case 'TOGGLE_PLAYER':
-        // Toggle active/inactive status of player with payload name
-        let newPlayers = state.players.map(p => {
-            let newPlayer = p;
-            if(p.name == action.payload){
-                newPlayer.active = !newPlayer.active;
-            }
-            return newPlayer;
-        })
-        return {...state,
-            players: newPlayers,
-        };
-    case 'TOGGLE_GAME_RESULT':
-        // Swap winner of a finished game
-        let newRounds = _.cloneDeep(state.rounds);
-        let winnerBefore = newRounds[action.payload].winner
-        if(winnerBefore>=0)
-          newRounds[action.payload].winner = winnerBefore == 0 ? 1 : 0
-        return {...state, rounds: newRounds, lastChange: Date.now()}
-    case 'REMOVE_GAME':
-        // Remove a game (can be played or unplayed)
-        let newState = _.cloneDeep(state);
-        newState.games.splice(action.payload,1)
-        if(newState.rounds.length > action.payload){
-          newState.rounds.splice(action.payload,1)
-        }
-        newState.lastChange = Date.now()
-        return newState
-    default:
-      return state
+const matchSlice = createSlice({
+  name: 'match',
+  initialState: INITIAL_STATE,
+  reducers: {
+    reset(state){
+      state.rounds = []; 
+      state.lastChange = -1; 
+      state.matchId = -1
+    },
+    startMatch: withTime<{players: Player[], games: string[]}>((state, action) => {
+      return {
+        players: action.payload.players,
+        games: action.payload.games,
+        rounds: [],
+        matchId: action.payload.time,
+        lastChange: action.payload.time,
+      }
+    }),
+    loadMatch(state, action: PayloadAction<State>){
+      return action.payload
+    },
+    gameResult: withTime<{winnerIndex: 0|-1|1}>((state, {payload}) => {
+      state.rounds[state.rounds.length-1].winner = payload.winnerIndex
+      state.lastChange = payload.time
+    }),
+    startNextGame: withTime<{teams: [Player[], Player[]]}>((state, {payload: {teams, time}}) => {
+      state.rounds.push({
+        teams,
+        game: state.games[state.rounds.length],
+        winner: -1
+      })
+      state.lastChange = time
+    }),
+    togglePlayer(state, action: PayloadAction<string>){
+      const player = state.players.find(x => x.name == action.payload)
+      if(player){
+        player.active = !player.active
+      }
+    },
+    toggleGameResult: withTime<{roundIndex: number}>((state, { payload: {roundIndex, time}}) => {
+      const round = state.rounds[roundIndex]
+      round.winner = round.winner == 0 ? 1 : 0
+      state.lastChange = time
+    }),
+    removeGame: withTime<{roundIndex: number}>((state, {payload: {roundIndex, time}}) => {
+      state.games.splice(roundIndex, 1)
+      if(state.rounds.length > roundIndex){
+        state.rounds.splice(roundIndex, 1)
+      }
+      state.lastChange = time
+    })
   }
-};
+})
 
-const persistedReducer = persistReducer(persistConfig, matchReducer)
+declare const dispatch: import("@reduxjs/toolkit").Dispatch;
+//dispatch(matchSlice.actions.startMatch({  }))
 
-export const store = createStore(persistedReducer);
-export const persistor = persistStore(store);
+const persistedReducer = persistReducer(persistConfig, matchSlice.reducer)
+
+export const { actions } = matchSlice
+export const store = createStore(persistedReducer)
+export const persistor = persistStore(store)
