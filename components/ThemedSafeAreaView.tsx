583183b9a02217e6961858e6bcb44840f2c882c5
Markus J. Ankenbrand <markus.ankenbrand@uni-wuerzburg.de>
2026-04-09 14:42:19 +0200

    Migrate SafeAreaView

--- components/ThemedSafeAreaView.tsx
+++ components/ThemedSafeAreaView.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { withStyles } from '@ui-kitten/components';
-import { SafeAreaView } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const StyledSafeAreaView = (props: any) => {
   const { eva, style, ...restProps } = props;
